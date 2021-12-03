dc63ac33dfd74a075f3d27ff2c7d93e4dfec0099
hwec0112 <74725096+hwec0112@users.noreply.github.com>
2021-12-02 21:29:39 -0800

    add map view page

---
+++ map/js/map.js
@@ -0,0 +1,673 @@
+const countryListAlpha3 = {
+  "AFG": "Afghanistan",
+  "ALB": "Albania",
+  "DZA": "Algeria",
+  "ASM": "American Samoa",
+  "AND": "Andorra",
+  "AGO": "Angola",
+  "AIA": "Anguilla",
+  "ATA": "Antarctica",
+  "ATG": "Antigua and Barbuda",
+  "ARG": "Argentina",
+  "ARM": "Armenia",
+  "ABW": "Aruba",
+  "AUS": "Australia",
+  "AUT": "Austria",
+  "AZE": "Azerbaijan",
+  "BHS": "Bahamas",
+  "BHR": "Bahrain",
+  "BGD": "Bangladesh",
+  "BRB": "Barbados",
+  "BLR": "Belarus",
+  "BEL": "Belgium",
+  "BLZ": "Belize",
+  "BEN": "Benin",
+  "BMU": "Bermuda",
+  "BTN": "Bhutan",
+  "BOL": "Bolivia, Plurinational States of",
+  "BES": "Bonaire, Sint Eustatius and Saba",
+  "BIH": "Bosnia and Herzegovina",
+  "BWA": "Botswana",
+  "BVT": "Bouvet Island",
+  "BRA": "Brazil",
+  "IOT": "British Indian Ocean Territor",
+  "BRN": "Brunei Darussalam",
+  "BGR": "Bulgaria",
+  "BFA": "Burkina Faso",
+  "BDI": "Burundi",
+  "CPV": "Cabo Verde",
+  "KHM": "Cambodia",
+  "CMR": "Cameroon",
+  "CAN": "Canada",
+  "CYM": "Cayman Islands",
+  "CAF": "Central African Republic",
+  "TCD": "Chad",
+  "CHL": "Chile",
+  "CHN": "China",
+  "CXR": "Christmas Island",
+  "CCK": "Cocos (Keeling) Islands",
+  "COL": "Colombia",
+  "COM": "Comoros",
+  "COD": "Congo, The Democratic Republic of the",
+  "COG": "Congo",
+  "COK": "Cook Islands",
+  "CRI": "Costa Rica",
+  "HRV": "Croatia",
+  "CUB": "Cuba",
+  "CUW": "Curaçao",
+  "CYP": "Cyprus",
+  "CZE": "Czechia",
+  "CIV": "Côte d'Ivoire",
+  "DNK": "Denmark",
+  "DJI": "Djibouti",
+  "DMA": "Dominica",
+  "DOM": "Dominican Republic",
+  "ECU": "Ecuador",
+  "EGY": "Egypt",
+  "SLV": "El Salvador",
+  "GNQ": "Equatorial Guinea",
+  "ERI": "Eritrea",
+  "EST": "Estonia",
+  "SWZ": "Eswatini",
+  "ETH": "Ethiopia",
+  "FLK": "Falkland Islands (Malvinas)",
+  "FRO": "Faroe Islands",
+  "FJI": "Fiji",
+  "FIN": "Finland",
+  "FRA": "France",
+  "GUF": "French Guiana",
+  "PYF": "French Polynesia",
+  "ATF": "French Southern Territories",
+  "GAB": "Gabon",
+  "GMB": "Gambia",
+  "GEO": "Georgia",
+  "DEU": "Germany",
+  "GHA": "Ghana",
+  "GIB": "Gibraltar",
+  "GRC": "Greece",
+  "GRL": "Greenland",
+  "GRD": "Grenada",
+  "GLP": "Guadeloupe",
+  "GUM": "Guam",
+  "GTM": "Guatemala",
+  "GGY": "Guernsey",
+  "GIN": "Guinea",
+  "GNB": "Guinea-Bissau",
+  "GUY": "Guyana",
+  "HTI": "Haiti",
+  "HMD": "Heard Island and McDonald Islands",
+  "VAT": "Holy See (Vatican City State)",
+  "HND": "Honduras",
+  "HKG": "Hong Kong",
+  "HUN": "Hungary",
+  "ISL": "Iceland",
+  "IND": "India",
+  "IDN": "Indonesia",
+  "IRN": "Iran, Islamic Republic of",
+  "IRQ": "Iraq",
+  "IRL": "Ireland",
+  "IMN": "Isle of Man",
+  "ISR": "Israel",
+  "ITA": "Italy",
+  "JAM": "Jamaica",
+  "JPN": "Japan",
+  "JEY": "Jersey",
+  "JOR": "Jordan",
+  "KAZ": "Kazakhstan",
+  "KEN": "Kenya",
+  "KIR": "Kiribati",
+  "PRK": "Korea, Democratic People's Republic of",
+  "KOR": "Korea, Republic of",
+  "KWT": "Kuwait",
+  "KGZ": "Kyrgyzstan",
+  "LAO": "Lao People's Democratic Republic",
+  "LVA": "Latvia",
+  "LBN": "Lebanon",
+  "LSO": "Lesotho",
+  "LBR": "Liberia",
+  "LBY": "Libya",
+  "LIE": "Liechtenstein",
+  "LTU": "Lithuania",
+  "LUX": "Luxembourg",
+  "MAC": "Macao",
+  "MDG": "Madagascar",
+  "MWI": "Malawi",
+  "MYS": "Malaysia",
+  "MDV": "Maldives",
+  "MLI": "Mali",
+  "MLT": "Malta",
+  "MHL": "Marshall Islands",
+  "MTQ": "Martinique",
+  "MRT": "Mauritania",
+  "MUS": "Mauritius",
+  "MYT": "Mayotte",
+  "MEX": "Mexico",
+  "FSM": "Micronesia, Federated States of",
+  "MDA": "Moldova",
+  "MCO": "Monaco",
+  "MNG": "Mongolia",
+  "MNE": "Montenegro",
+  "MSR": "Montserrat",
+  "MAR": "Morocco",
+  "MOZ": "Mozambique",
+  "MMR": "Myanmar",
+  "NAM": "Namibia",
+  "NRU": "Nauru",
+  "NPL": "Nepal",
+  "NLD": "Netherlands",
+  "NCL": "New Caledonia",
+  "NZL": "New Zealand",
+  "NIC": "Nicaragua",
+  "NER": "Niger",
+  "NGA": "Nigeria",
+  "NIU": "Niue",
+  "NFK": "Norfolk Island",
+  "MNP": "Northern Mariana Islands",
+  "NOR": "Norway",
+  "OMN": "Oman",
+  "PAK": "Pakistan",
+  "PLW": "Palau",
+  "PSE": "Palestine, State of",
+  "PAN": "Panama",
+  "PNG": "Papua New Guinea",
+  "PRY": "Paraguay",
+  "PER": "Peru",
+  "PHL": "Philippines",
+  "PCN": "Pitcairn",
+  "POL": "Poland",
+  "PRT": "Portugal",
+  "PRI": "Puerto Rico",
+  "QAT": "Qatar",
+  "MKD": "Republic of North Macedonia",
+  "ROU": "Romania",
+  "RUS": "Russian Federation",
+  "RWA": "Rwanda",
+  "REU": "Réunion",
+  "BLM": "Saint Barthélemy",
+  "SHN": "Saint Helena, Ascension and Tristan da Cunha",
+  "KNA": "Saint Kitts and Nevis",
+  "LCA": "Saint Lucia",
+  "MAF": "Saint Martin (French part)",
+  "SPM": "Saint Pierre and Miquelon",
+  "VCT": "Saint Vincent and the Grenadines",
+  "WSM": "Samoa",
+  "SMR": "San Marino",
+  "STP": "Sao Tome and Principe",
+  "SAU": "Saudi Arabia",
+  "SEN": "Senegal",
+  "SRB": "Serbia",
+  "SYC": "Seychelles",
+  "SLE": "Sierra Leone",
+  "SGP": "Singapore",
+  "SXM": "Sint Maarten (Dutch part)",
+  "SVK": "Slovakia",
+  "SVN": "Slovenia",
+  "SLB": "Solomon Islands",
+  "SOM": "Somalia",
+  "ZAF": "South Africa",
+  "SGS": "South Georgia and the South Sandwich Islands",
+  "SSD": "South Sudan",
+  "ESP": "Spain",
+  "LKA": "Sri Lanka",
+  "SDN": "Sudan",
+  "SUR": "Suriname",
+  "SJM": "Svalbard and Jan Mayen",
+  "SWE": "Sweden",
+  "CHE": "Switzerland",
+  "SYR": "Syrian Arab Republic",
+  "TWN": "Taiwan, Province of China",
+  "TJK": "Tajikistan",
+  "TZA": "Tanzania, United Republic of",
+  "THA": "Thailand",
+  "TLS": "Timor-Leste",
+  "TGO": "Togo",
+  "TKL": "Tokelau",
+  "TON": "Tonga",
+  "TTO": "Trinidad and Tobago",
+  "TUN": "Tunisia",
+  "TUR": "Turkey",
+  "TKM": "Turkmenistan",
+  "TCA": "Turks and Caicos Islands",
+  "TUV": "Tuvalu",
+  "UGA": "Uganda",
+  "UKR": "Ukraine",
+  "ARE": "United Arab Emirates",
+  "GBR": "United Kingdom of Great Britain and Northern Ireland",
+  "UMI": "United States Minor Outlying Islands",
+  "USA": "United States",
+  "URY": "Uruguay",
+  "UZB": "Uzbekistan",
+  "VUT": "Vanuatu",
+  "VEN": "Venezuela, Bolivarian Republic of",
+  "VNM": "Viet Nam",
+  "VGB": "Virgin Islands (British)",
+  "VIR": "Virgin Islands (U.S.)",
+  "WLF": "Wallis and Futuna",
+  "ESH": "Western Sahara",
+  "YEM": "Yemen",
+  "ZMB": "Zambia",
+  "ZWE": "Zimbabwe",
+  "ALA": "Åland Islands"
+};
+
+
+// set the dimensions and margins of the graph
+var margin = {top: 40, right: 10, bottom: 40, left: 100},
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+// The svg
+var svg = d3.select("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")")
+// zoom
+// function zoomed(event) {
+//   console.log(event);
+//   const {transform} = event;
+//   g.attr("transform", transform);
+//   g.attr("stroke-width", 1 / transform.k);
+// }
+
+// const zoom = d3.zoom()
+//   .scaleExtent([1, 8])
+//   .on("zoom", zoomed);
+
+// create a tooltip
+var tooltip = d3.select("#tooltip")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "1px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+  .style("position", "absolute")
+      
+// Map and projection
+//var path = d3.geoPath();
+var projection = d3.geoMercator()
+  .scale(100)
+  .center([0,20])
+  .translate([width / 2 - margin.left, height / 2]);
+
+// Data and color scale
+geo_data = d3.map()
+glo_topo = undefined
+var data = []
+var data_val = []
+// 0: "Name"
+// 1: "Mammals"
+// 2: "Birds"
+// 3: "Reptiles"
+// 4: "Amphibians"
+// 5: "Fishes"
+// 6: "Molluscs"
+// 7: "Other"
+// 8: "Plants"
+// 9: "Fungi"
+// 10: "Chromists"
+// 11: "Total"
+for (let i = 0; i < 11; i++){
+  data.push(d3.map())
+  data_val.push([])
+}
+
+cur_idx = 10
+
+var domain = [0, 1000, 2000, 3000, 4000]
+var labels = ["NA", "0 - 1000", "1000 - 2000", "2000 - 3000", "3000 - 4000"]
+var range = ["#F5F5F5", "#3BCA6D","#77945C","#B25F4A", "#ED2938"]
+var colorScale = d3.scaleThreshold()
+  .domain(domain)
+  .range(range);
+
+var promises = []
+promises.push(d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson"));
+// promises.push(d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function(d) { data.set(d.code, +d.pop); }))
+promises.push(d3.csv("https://raw.githubusercontent.com/hwec0112/726project/main/data/t_5.csv", function(d) { 
+                dkeys = Object.keys(d)
+                for (let i = 0; i < dkeys.length-1; i++){
+                  data[i].set(d.Name, d[dkeys[i+1]])
+                  data_val[i].push(d[dkeys[i+1]]);
+                }
+}));
+
+// y = d3.scaleLinear()
+//     .domain(d3.extent(data[10], d => d.value)).nice()
+//     .range([height - margin.bottom, margin.top])
+// color = d3.scaleSequential(y.domain(), d3.interpolateTurbo)
+// console.log(color)
+// console.log(data[10].values())
+
+const select = document.getElementById('class_radio');
+
+function make_map(){
+  svg.select("g").remove();
+  var topo = glo_topo[0]
+  let mouseOver = function(d) {
+      if (!(data[cur_idx].get(countryListAlpha3[d.id]) === undefined)){
+        d3.selectAll(".topo")
+        .transition()
+        .duration(200)
+        .style("opacity", .5)
+      
+        d3.select(this)
+        //.filter(function(d){d.total = data.get(d.id) || 0; return d.total <= max_pop && d.total >= min_pop})
+        .transition()
+          .duration(200)
+          .style("opacity", 1)
+          .style("stroke", "black")
+        
+        d.total = data[cur_idx].get(countryListAlpha3[d.id]) || -1;
+
+        tooltip
+          .style("opacity", 0.8)
+          .html(countryListAlpha3[d.id] + ": " + d3.format(",.2r")(d.total))
+          .style("left", (d3.event.pageX) + "px")		
+          .style("top", (d3.event.pageY - 28) + "px");
+      }
+  }
+
+  //   // create continuous color legend
+  // function continuous(selector_id, colorscale) {
+  //   var legendheight = 200,
+  //       legendwidth = 80,
+  //       margin = {top: 10, right: 60, bottom: 10, left: 2};
+
+  //   var canvas = d3.select(selector_id)
+  //     .style("height", legendheight + "px")
+  //     .style("width", legendwidth + "px")
+  //     .style("position", "relative")
+  //     .append("canvas")
+  //     .attr("height", legendheight - margin.top - margin.bottom)
+  //     .attr("width", 1)
+  //     .style("height", (legendheight - margin.top - margin.bottom) + "px")
+  //     .style("width", (legendwidth - margin.left - margin.right) + "px")
+  //     .style("border", "1px solid #000")
+  //     .style("position", "absolute")
+  //     .style("top", (margin.top) + "px")
+  //     .style("left", (margin.left) + "px")
+  //     .node();
+
+  //   var ctx = canvas.getContext("2d");
+
+  //   var legendscale = d3.scaleLinear()
+  //     .range([1, legendheight - margin.top - margin.bottom])
+  //     .domain(colorscale.domain());
+
+  //   // image data hackery based on http://bl.ocks.org/mbostock/048d21cf747371b11884f75ad896e5a5
+  //   var image = ctx.createImageData(1, legendheight);
+  //   d3.range(legendheight).forEach(function(i) {
+  //     var c = d3.rgb(colorscale(legendscale.invert(i)));
+  //     image.data[4*i] = c.r;
+  //     image.data[4*i + 1] = c.g;
+  //     image.data[4*i + 2] = c.b;
+  //     image.data[4*i + 3] = 255;
+  //   });
+  //   ctx.putImageData(image, 0, 0);
+
+  //   // A simpler way to do the above, but possibly slower. keep in mind the legend width is stretched because the width attr of the canvas is 1
+  //   // See http://stackoverflow.com/questions/4899799/whats-the-best-way-to-set-a-single-pixel-in-an-html5-canvas
+  //   /*
+  //   d3.range(legendheight).forEach(function(i) {
+  //     ctx.fillStyle = colorscale(legendscale.invert(i));
+  //     ctx.fillRect(0,i,1,1);
+  //   });
+  //   */
+
+  //   var legendaxis = d3.axisRight()
+  //     .scale(legendscale)
+  //     .tickSize(6)
+  //     .ticks(8);
+
+  //   var svg = d3.select(selector_id)
+  //     .append("svg")
+  //     .attr("height", (legendheight) + "px")
+  //     .attr("width", (legendwidth) + "px")
+  //     .style("position", "absolute")
+  //     .style("left", "0px")
+  //     .style("top", "0px")
+
+  //   svg
+  //     .append("g")
+  //     .attr("class", "axis")
+  //     .attr("transform", "translate(" + (legendwidth - margin.left - margin.right + 3) + "," + (margin.top) + ")")
+  //     .call(legendaxis);
+  // };
+
+  let mouseLeave = function(d) {
+    d3.selectAll(".topo")
+      .transition()
+      .duration(0)
+      .style("opacity", .7)
+      
+    d3.selectAll(".topo")
+      .transition()
+      .duration(200)
+      .style("stroke", "white")
+      
+    tooltip
+          .style("opacity", 0)
+  }
+
+  mx_data = Math.max(...data_val[cur_idx])
+  mn_data = Math.min(...data_val[cur_idx])
+  var scale = d3.scaleLinear()
+  .domain([mn_data, mx_data])
+  .range([0, 1000]);
+  scale_d = [scale.domain()[0], (scale.domain()[0]+scale.domain()[1])/2, scale.domain()[1]]
+
+  color = d3.scaleSqrt(scale_d, ["green", "yellow", "red"])
+  // color = d3.scaleSequential(scale_d, d3.interpolateRgb("green", "red"))
+  // Draw the map
+  svg.append("g")
+    .selectAll("path")
+    .data(topo.features)
+    .enter()
+    .append("path")
+    .attr("class", "topo")
+      // draw each country
+      .attr("d", d3.geoPath()
+        .projection(projection)
+      )
+      // set the color of each country
+      .attr("fill", function (d) {
+        // console.log(d);
+        d.total = data[cur_idx].get(countryListAlpha3[d.id]) || -1;
+        // console.log("403", data[cur_idx].values())
+        // console.log(d.total, color(d.total))
+        return color(d.total);
+      })
+      .style("opacity", .7)
+    .on("mouseover", mouseOver )
+    .on("mouseleave", mouseLeave )
+
+  legend({
+      color: color,
+      title: "Sepecies count"
+  })
+  // var legend_x = width - margin.left
+  // var legend_y = height - 60
+  // svg.append("g")
+  //   .attr("class", "legendQuant")
+  //   .attr("transform", "translate(" + legend_x + "," + legend_y+")");
+
+  // var legend = d3.legendColor()
+  //   .labels(labels)
+  //   .title("")
+  //   .scale(scale);
+  // svg.select(".legendQuant")
+  //   .call(legend);
+}
+
+select.addEventListener('click', ({ target }) => { // handler fires on root container click
+  console.log("target value", target.value)
+  if (!(target.value === undefined)){
+    cur_idx = target.value
+    console.log(data[target.value])
+    make_map()
+  }
+});
+
+myDataPromises = Promise.all(promises).then(function(topo) {
+  glo_topo = topo
+	make_map()
+  // svg.call(zoom);
+})
+
+function legend({
+  color,
+  title,
+  tickSize = 6,
+  width = 320,
+  height = 44 + tickSize,
+  marginTop = 18,
+  marginRight = 0,
+  marginBottom = 16 + tickSize,
+  marginLeft = 0,
+  ticks = width / 64,
+  tickFormat,
+  tickValues
+} = {}) {
+  const svg = d3.select("#legend")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .style("overflow", "visible")
+    .style("display", "block");
+
+  svg.select("g").remove();
+
+  let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+  let x;
+
+  // Continuous
+  if (color.interpolate) {
+    const n = Math.min(color.domain().length, color.range().length);
+
+    x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+  }
+
+  // Sequential
+  else if (color.interpolator) {
+    x = Object.assign(color.copy()
+      .interpolator(d3.interpolateRound(marginLeft, width - marginRight)), {
+        range() {
+          return [marginLeft, width - marginRight];
+        }
+      });
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== "function") {
+        tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+      }
+    }
+  }
+
+  // Threshold
+  else if (color.invertExtent) {
+    const thresholds = color.thresholds ? color.thresholds() // scaleQuantize
+      :
+      color.quantiles ? color.quantiles() // scaleQuantile
+      :
+      color.domain(); // scaleThreshold
+
+    const thresholdFormat = tickFormat === undefined ? d => d :
+      typeof tickFormat === "string" ? d3.format(tickFormat) :
+      tickFormat;
+
+    x = d3.scaleLinear()
+      .domain([-1, color.range().length - 1])
+      .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.range())
+      .join("rect")
+      .attr("x", (d, i) => x(i - 1))
+      .attr("y", marginTop)
+      .attr("width", (d, i) => x(i) - x(i - 1))
+      .attr("height", height - marginTop - marginBottom)
+      .attr("fill", d => d);
+
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+  }
+
+  // Ordinal
+  else {
+    x = d3.scaleBand()
+      .domain(color.domain())
+      .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.domain())
+      .join("rect")
+      .attr("x", x)
+      .attr("y", marginTop)
+      .attr("width", Math.max(0, x.bandwidth() - 1))
+      .attr("height", height - marginTop - marginBottom)
+      .attr("fill", color);
+
+    tickAdjust = () => {};
+  }
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x)
+      .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+      .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+      .tickSize(tickSize)
+      .tickValues(tickValues))
+    .call(tickAdjust)
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+      .attr("x", marginLeft)
+      .attr("y", marginTop + marginBottom - height - 6)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text(title));
+
+  return svg.node();
+}
+
+function ramp(color, n = 256) {
+  var canvas = document.createElement('canvas');
+  canvas.width = n;
+  canvas.height = 1;
+  const context = canvas.getContext("2d");
+  for (let i = 0; i < n; ++i) {
+    context.fillStyle = color(i / (n - 1));
+    context.fillRect(i, 0, 1, 1);
+  }
+  return canvas;
+}
+
+
+ 
